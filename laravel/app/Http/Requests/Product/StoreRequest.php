8327b368d29e6fc510bb4ae43e9b9cc4aabd3a28
IvanMareev <rain12003@yandex.ru>
2025-12-23 21:21:03 +1000

    12
    Signed-off-by: IvanMareev <rain12003@yandex.ru>

---
+++ laravel/app/Http/Requests/Product/StoreRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use App\Enums\ProductStatus;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Enum;
+
+class StoreRequest extends ApiRequest
+{
+
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string'],
+            'description' => [ 'string'],
+            'price' => ['required', 'numeric', 'min:1', 'max:10000000'],
+            'count' => ['required', 'integer', 'min:1', 'max:100000'],
+            'status' => ['required', new Enum(ProductStatus::class)],
+            'images*.' => ['image'],
+        ];
+    }
+}
