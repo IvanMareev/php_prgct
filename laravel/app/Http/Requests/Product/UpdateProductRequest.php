a55ef6562016fcf8c133d502c6f838daf2d3f57f
IvanMareev <rain12003@yandex.ru>
2026-01-13 07:31:19 +1000

    12
    Signed-off-by: IvanMareev <rain12003@yandex.ru>

---
+++ laravel/app/Http/Requests/Product/UpdateProductRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Requests\Product;
+
+use App\Http\Requests\ApiRequest;
+
+class UpdateProductRequest extends ApiRequest
+{
+    public function rules(): array
+    {
+        return [
+            'name'        => ['sometimes', 'string'],
+            'description' => ['sometimes', 'nullable', 'string'],
+            'price'       => ['sometimes', 'numeric', 'min:1'],
+            'count'       => ['sometimes', 'integer', 'min:0'],
+            'status'      => ['sometimes'],
+        ];
+    }
+}
