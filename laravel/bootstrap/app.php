d0f1bb0283c007435b00ab3b876e9204d7bb633d
Ivan M <165126155+IvanMareev@users.noreply.github.com>
2026-02-05 11:45:47 +0000

    1

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -38,7 +38,7 @@ $app->singleton(
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\JsonExceptionHandler::class
+    App\Exceptions\Handler::class
 );
 
 
